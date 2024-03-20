581af5ce51f3c7058d06d29abe86cea9a5387c43
cairo <101215230+cairoeth@users.noreply.github.com>
2024-03-19 20:10:43 -0500

    🔨 Wombo Combo deployment function

--- contracts/contracts/levels/WomboComboFactory.sol
+++ contracts/contracts/levels/WomboComboFactory.sol
@@ -8,8 +8,18 @@ contract DummyFactory is Level {
     function createInstance(
         address _player
     ) public payable override returns (address) {
-        // _player;
-        // return address(new Dummy());
+        Token token = new Token("Staking", "STK", 100 * 10 ** 18);
+        Token reward = new Token("Reward", "RWD", 100_000_000 * 10 ** 18);
+
+        Forwarder forwarder = new Forwarder();
+
+        Staking staking = new Staking(token, reward, address(forwarder));
+
+        staking.setRewardsDuration(20);
+        reward.transfer(address(staking), reward.totalSupply());
+        token.transfer(_player, token.totalSupply());
+
+        return address(staking);
     }
 
     function validateInstance(
